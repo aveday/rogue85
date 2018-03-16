3e1594b2ee6bd97890fb1ddc433c85919a84909d
David Williams <david@aveday.net>
2018-03-16 20:55:05 +1100

    Balance room splitting chances

--- src/main.c
+++ src/main.c
@@ -68,10 +68,10 @@ typedef struct {
   uint8_t corner2;
 } room_t;
 
-uint8_t cxy(uint8_t x, uint8_t y) { return WIDTH * y + x; }
-uint8_t cyx(uint8_t y, uint8_t x) { return WIDTH * y + x; }
 
 bool split_room(room_t rooms[]) {
+  uint8_t cxy(uint8_t x, uint8_t y) { return WIDTH * y + x; }
+  uint8_t cyx(uint8_t y, uint8_t x) { return WIDTH * y + x; }
   
   uint8_t id;
   uint8_t c11, c12, c21, c22;
@@ -79,7 +79,7 @@ bool split_room(room_t rooms[]) {
   uint8_t split;
 
   // pick which room to split
-  for (id = 0; id < MAX_ROOMS; ++id) {
+  for (id = 0; id < MAX_ROOMS - 1; ++id) {
     room_t room = rooms[id];
 
     if (room.corner1 > room.corner2)
@@ -94,21 +94,11 @@ bool split_room(room_t rooms[]) {
       continue;
 
     bool v = too_short || (rand() % (width + height) < width && !too_narrow);
-    if (v) {
-      // vertical split
-      c11 = room.corner1 % WIDTH;
-      c21 = room.corner1 / WIDTH;
-      c12 = room.corner2 % WIDTH;
-      c22 = room.corner2 / WIDTH;
-      cp = cxy;
-    } else {
-      // horizontal split
-      c11 = room.corner1 / WIDTH;
-      c21 = room.corner1 % WIDTH;
-      c12 = room.corner2 / WIDTH;
-      c22 = room.corner2 % WIDTH;
-      cp = cyx;
-    }
+    c11 = v ? room.corner1 % WIDTH : room.corner1 / WIDTH;
+    c21 = v ? room.corner1 / WIDTH : room.corner1 % WIDTH;
+    c12 = v ? room.corner2 % WIDTH : room.corner2 / WIDTH;
+    c22 = v ? room.corner2 / WIDTH : room.corner2 % WIDTH;
+    cp  = v ? cxy : cyx;
 
     split = c11 + MIN + rand() % (c12 - c11 + 1 - 2*MIN);
 
@@ -123,18 +113,27 @@ bool split_room(room_t rooms[]) {
       continue;
     break;
   }
-  if (id == MAX_ROOMS)
+
+  if (id == MAX_ROOMS - 1)
     return false;
 
-  uint8_t next = 0;
+  uint8_t next = id + 1;
   while (next < MAX_ROOMS && rooms[next].corner1 <= rooms[next].corner2)
     ++next;
   if (next == MAX_ROOMS)
     return false;
 
-  rooms[next].corner2 = rooms[id].corner2;
-  rooms[next].corner1 = cp(split + 1, c21);
-  rooms[id].corner2   = cp(split - 1, c22);
+  uint8_t nc1 = cp(split + 1, c21);
+  uint8_t nc2 = cp(split - 1, c22);
+  if (rand() % 2) {
+    rooms[next].corner1 = nc1;
+    rooms[next].corner2 = rooms[id].corner2;
+    rooms[id].corner2   = nc2;
+  } else {
+    rooms[next].corner1 = rooms[id].corner1;
+    rooms[next].corner2 = nc2;
+    rooms[id].corner1   = nc1;
+  }
 
   uint8_t door = c21 + rand() % (c22 - c21 + 1);
 
@@ -152,12 +151,12 @@ void build_level() {
   room_t rooms[MAX_ROOMS];
   for (int i = 0; i < MAX_ROOMS; ++i) {
     // rooms unused if corner1 > corner2
-    rooms[i].corner1 = cxy(WIDTH - 1, HEIGHT - 1);
+    rooms[i].corner1 = 1;
     rooms[i].corner2 = 0;
   }
 
   rooms[0].corner1 = 0;
-  rooms[0].corner2 = cxy(WIDTH - 1, HEIGHT - 1);
+  rooms[0].corner2 = (WIDTH - 1) + WIDTH * (HEIGHT - 1);
   while (split_room(rooms)) continue;
 
   // add player
